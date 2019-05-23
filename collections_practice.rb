90f6b1a6cb92d7ddba6a017c2e0aaefc3a26ad1d
Alexander Schelchere <ascellie@gmail.com>
2019-05-23 09:40:09 -0400

    outline for methods

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,41 @@
-# your code goes here
+require "pry"
+
+# iterate - first instance where "r" is not first returns false
+def begins_with_r
+
+end
+
+# select
+def contain_a
+
+end
+
+# find
+def first_wa
+
+end
+
+# select
+def remove_non_strings
+
+end
+
+# iterate and increment
+def count_elements
+
+end
+
+# map/collect
+def merge_data
+
+end
+
+# find
+def find_cool
+
+end
+
+# sorting
+def organize_schools
+
+end
